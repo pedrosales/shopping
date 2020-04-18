bf4e041fa99c581ff97ce23f77d620447b5869c5
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-18 19:25:28 -0300

    product detail page

--- lib/pages/product.page.dart
+++ lib/pages/product.page.dart
@@ -24,7 +24,40 @@ class ProductPage extends StatelessWidget {
             ),
           ];
         },
-        body: Container(),
+        body: ListView(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(
+                top: 10,
+                left: 10,
+                right: 10,
+              ),
+              child: Text(
+                "Dry Fit Long Sleeve",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text("by Nike"),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                "Details",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                  "Nike Dri-Fit is a polyester fabric designed to help you keep dry so you can more comfortably work harder, longer"),
+            ),
+          ],
+        ),
       ),
     );
   }
