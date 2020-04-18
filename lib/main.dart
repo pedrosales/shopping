f68720fa6eda010eed5708bb544a3a46c2a0be08
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-18 10:56:59 -0300

    inicio login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping/pages/login.page.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,50 +9,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Meu App"),
-      ),
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: Colors.red,
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          // mainAxisAlignment: MainAxisAlignment.spaceAround,
-          // crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              color: Colors.yellow,
-              child: Text("item 1"),
-            ),
-            Text("item 2"),
-            Text("item 3"),
-            Text("item 4"),
-            Text("item 4"),
-            Text("item 4"),
-            Text("item 4"),
-            Text("item 4"),
-            Text("item 4"),
-            Text("item 4"),
-            Text("item 4"),
-            Text("item 4"),
-            Text("item 4"),
-            Text("item 4"),
-          ],
-        ),
-      ),
+      home: LoginPage(),
     );
   }
 }
---
+++ lib/pages/login.page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 80,
+          left: 20,
+          right: 20,
+          bottom: 40,
+        ),
+        child: Text("Ola"),
+      ),
+    );
+  }
+}
