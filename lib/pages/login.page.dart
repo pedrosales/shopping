a70fabc627b318f882868296164507d085d892b2
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-18 11:23:05 -0300

    página de login

--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -5,13 +5,31 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        color: Color(0xFFF5F5F5),
         padding: EdgeInsets.only(
           top: 80,
           left: 20,
           right: 20,
           bottom: 40,
         ),
-        child: Text("Ola"),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 450,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  new BoxShadow(
+                    color: Colors.black12,
+                    offset: new Offset(1, 2.0),
+                    blurRadius: 5,
+                    spreadRadius: 1,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
