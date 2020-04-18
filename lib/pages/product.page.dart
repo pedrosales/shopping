37c954a2b591c0a1b16f5854c4b124ea0617b497
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-18 19:13:21 -0300

    Sliver app bar

--- lib/pages/product.page.dart
+++ lib/pages/product.page.dart
@@ -4,12 +4,27 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        color: Colors.blue,
-        child: Center(
-          child: Text("Detalhes do produto"),
-        ),
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsCrolled) {
+          return <Widget>[
+            SliverAppBar(
+              backgroundColor: Colors.white.withOpacity(0),
+              elevation: 0.0,
+              expandedHeight: 500.0,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                background: Image.asset(
+                  "assets/product-10.png",
+                  width: double.infinity,
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+          ];
+        },
+        body: Container(),
       ),
     );
   }
