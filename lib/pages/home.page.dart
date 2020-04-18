21d8b26cad645cffd502572cc35f67afd43c2caa
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-18 19:04:11 -0300

    detalhes do produto on tap

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping/pages/product.page.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -50,7 +51,7 @@ class HomePage extends StatelessWidget {
             ),
             Container(
               height: 280,
-              child: productList(),
+              child: productList(context),
             ),
           ],
         ),
@@ -139,22 +140,22 @@ Widget categoryItem() {
   );
 }
 
-Widget productList() {
+Widget productList(BuildContext context) {
   return Container(
     child: ListView(
       scrollDirection: Axis.horizontal,
       children: <Widget>[
-        productItem(),
-        productItem(),
-        productItem(),
-        productItem(),
-        productItem(),
+        productItem(context),
+        productItem(context),
+        productItem(context),
+        productItem(context),
+        productItem(context),
       ],
     ),
   );
 }
 
-Widget productItem() {
+Widget productItem(BuildContext context) {
   return Container(
     padding: EdgeInsets.all(10),
     margin: EdgeInsets.all(5),
@@ -163,11 +164,21 @@ Widget productItem() {
     child: Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Image.asset(
-          "assets/product-1.png",
-          width: 170,
-          height: 170,
-          fit: BoxFit.cover,
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ProductPage(),
+              ),
+            );
+          },
+          child: Image.asset(
+            "assets/product-1.png",
+            width: 170,
+            height: 170,
+            fit: BoxFit.cover,
+          ),
         ),
         SizedBox(
           height: 10,
