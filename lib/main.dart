6a896feba95e77a87e9bbe5595fb7ae984ff0e7c
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-18 10:04:02 -0300

    Hello World

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meu App"),
+      ),
+      body: Container(
+        color: Colors.blue,
+        child: Center(
+          child: Text("Hello World"),
+        ),
+      ),
+    );
+  }
+}
